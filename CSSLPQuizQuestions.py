fa5dd0c05dadf1026c9f5298818e7f2705a83b1a
JHeath74 <JHeath74@msn.com>
2023-04-17 11:04:58 -0600

    Setting up Dict with Questions and answers

---
+++ CSSLPQuizQuestions.py
@@ -0,0 +1,12 @@
+thisdict = {
+  "Previous errors in the code base(s)": "What is the most important source of error information to employ when checking code?",
+  "A security design that cannot be bypassed or circumvented": "Complete mediation is an approach to security that includes what?",
+  "Code walk-throughs": "Which testing methodology can improve maintainability of the code base?",
+  "Least privilege":"What is the fundamental approach to security in which an object has only the necessary rights and privileges to perform its task with no additional permissions?",
+  "Custom cryptographic functions to avoid algorithm disclosure":"Elements of defensive coding include all of the following except what?",
+  "Access":"What describes the ability of a subject to interact with an object?",
+  "DAST":"Which of the following is the best method of finding race conditions?",
+  "Economy of mechanism":"Using the principle of keeping things simple is related to what?",
+  "Impact":"What is the essential element for scoring the severity of bugs/vulnerabilities?",
+  "A plan":"Qualification testing is always guided by what?",
+}
